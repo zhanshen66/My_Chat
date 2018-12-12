f7e6c4c2c5d1072cb952be47461e0585b5b9f74f
zhanshen66 <453082584@qq.com>
2018-12-13 00:24:28 +0800

    add first auth.py

--- my_chat/__init__.py
+++ my_chat/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import os
+from my_chat.blueprints.auth import auth_bp
 
 
 def create_app(config_name=None):
@@ -7,4 +8,10 @@ def create_app(config_name=None):
         config_name = os.getenv('FLASK_CONFIG', 'development')
     app = Flask('my_chat')
     #app.config.from_object(config[config_name])
+    register_blueprints(app)
+
     return app
+
+
+def register_blueprints(app):
+    app.register_blueprint(auth_bp)
---
+++ my_chat/blueprints/auth.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+auth_bp = Blueprint('auth',__name__)
+
+
+@auth_bp.route('/login')
+def login():
+    return 'this is login'
