a7bc146130d3178425bcec5e35bc80d477516d9d
zhanshen66 <453082584@qq.com>
2018-12-12 23:53:00 +0800

    add models

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,4 @@
 FLASK_APP=my_chat
+FLASK_ENV=development
 FLASK_RUN_HOST='0.0.0.0'
 FLASK_RUN_PORT=8006
--- my_chat/__init__.py
+++ my_chat/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import os
 
 
-def create_app():
+def create_app(config_name=None):
+    if config_name is None:
+        config_name = os.getenv('FLASK_CONFIG', 'development')
     app = Flask('my_chat')
+    #app.config.from_object(config[config_name])
     return app
